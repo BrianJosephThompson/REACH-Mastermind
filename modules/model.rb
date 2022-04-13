f40d7dd2a73d94cf381f516f1e4385fd06814fa2
BrianJosephThompson <95653082+BrianJosephThompson@users.noreply.github.com>
2022-04-12 21:14:17 -0700

    function names updated

--- modules/model.rb
+++ modules/model.rb
@@ -42,7 +42,7 @@ end
 
 # Class for storing player scores in an SQL database.
 # Retrieves scores from database, and sorts them by the highest score.
-class User
+class Users
   attr_accessor :id, :score, :username
 
   def initialize (array)
@@ -52,7 +52,7 @@ class User
   end
 
   # Inserts the players score inside the database.
-  def self.add_user(user_info)
+  def self.add_user_to_db(user_info)
     query = <<-REQUEST
       INSERT INTO #{$tablename} (score, username) VALUES ("#{user_info[:score]}",
               "#{user_info[:username]}")
@@ -62,7 +62,7 @@ class User
   end
 
   # Retrieves all rows stored inside the database, sorts by highest score.
-  def self.high_score_list
+  def self.get_high_score_list
     query = <<-REQUEST
       SELECT * FROM #{$tablename}
       ORDER BY score DESC
@@ -71,7 +71,7 @@ class User
     rows = ConnectionSQlite.new.execute(query)
     if rows.any?
       rows.collect do |row|
-        User.new(row)
+        Users.new(row)
       end
     else
       []
@@ -97,8 +97,8 @@ end
 
 
 def _main()
-  # p User.add_user(score: 11, username: "Batman")
-  #  User.high_score_list
+  # p Users.add_user(score: 11, username: "Batman")
+  #  Users.high_score_list
 end
 
 _main()
