e91028c2633bda80a46dfab7216190e948188ce8
BrianJosephThompson <95653082+BrianJosephThompson@users.noreply.github.com>
2022-04-14 15:53:12 -0700

    string formatting for url

--- code.rb
+++ code.rb
@@ -3,7 +3,7 @@ require 'net/http'
 
 # Class for generating secret code array for Mastermind game.
 class CodeGenerator
-  DIGITS_REQUIRED      = 4
+  DIGITS               = 4
   MIN_NUMBER           = 0
   MAX_NUMBER           = 7
   COLUMNS              = 1
@@ -38,17 +38,16 @@ class CodeGenerator
   # Retrieves a string based on the constants defined at the start of the class.
   # Converts string characters to integers and stuffs them into a class instance array.
   def get_code_from_api
-    uri = URI(
-      "https://www.random.org/integers/?
-      num=#{DIGITS_REQUIRED}&
+    string = "https://www.random.org/integers/?
+      num=#{DIGITS}&
       min=#{MIN_NUMBER}&
       max=#{MAX_NUMBER}&
       col=#{COLUMNS}&
       base=#{NUMBER_BASE}&
       format=#{FORMAT}&
       rnd#{RANDOM_REQUEST}&
-      ")
-
+      "
+    uri = URI(string)
     response = Net::HTTP.get_response(uri)
     if response.is_a?(Net::HTTPSuccess)
       temp = response.body 
@@ -60,7 +59,7 @@ class CodeGenerator
   # Stuffs class instance array with randomly generated integers 
   def generate_internal_code_array
     index = 0
-    while index < DIGITS_REQUIRED
+    while index < DIGITS
         @random_code_array << rand(MIN_NUMBER..MAX_NUMBER)
         index += 1
     end
