4d08474d7c8b636709f9633f01c12623029265d3
BrianJosephThompson <95653082+BrianJosephThompson@users.noreply.github.com>
2022-04-21 15:53:54 -0700

    Updates to use hashes instead of arrays/ nested loops

--- modules/gameplay.rb
+++ modules/gameplay.rb
@@ -12,6 +12,7 @@ module Gameplay
     @guess_array                = []
     @right_number_right_place   = 0
     @right_number_wrong_place   = 0
+    @tracking_hash              = Hash.new(0)
   end
 
 
@@ -42,6 +43,22 @@ module Gameplay
     end
   end
 
+  # Checks to see if player guess and code guess numbers match at a given digit position
+  # If they do, count it and add the index as key along with true as value to a hash.
+  # if they do not, add the index as key and the number as value to hash.
+  def check_well_placed_hash
+    index = 0
+    while index < DIGITS
+      if @guess_array[index] == @code_check_array[index]
+        @right_number_right_place += 1
+        @tracking_hash[index] = true
+      else
+        @tracking_hash[index] = @code_check_array[index]
+      end
+      index += 1
+    end
+  end
+
 
   # Checks a guess array element against each of the remaining code array elements.
   # If a match is made, it is counted and the block is exited to avoid double counting.
@@ -53,6 +70,23 @@ module Gameplay
           @right_number_wrong_place += 1
           break
         end
+      end
+      index += 1
+    end
+  end
+
+
+  # Checks guess array against hash created in check_well_placed_hash
+  # If the key for the hash has a value of true, we know those to be exact matches and skip them
+  # If the value for an index key is a match to the value at the guess arrays index,
+  # Then we know the value exists at a different index, so we incremement right number wrong place.
+  def check_misplaced_hash
+    index = 0
+    while index < DIGITS
+      if @tracking_hash[index] == true
+        #Skipping matches
+      elsif @tracking_hash.key?(index) && @tracking_hash.value?(@guess_array[index])
+        @right_number_wrong_place += 1
       end
       index += 1
     end
